56378ad4ec7d4df272d76a67eea2e4dff605196e
granadalook <tiven17.jsga@gmail.com>
2022-05-05 03:20:38 -0500

    FIRST COMMIT:Se inicia nuevo proyecto  con springboot

---
+++ back-crud-database-kata/src/main/java/com/example/cruddatabasekata/CrudDatabaseKataApplication.java
@@ -0,0 +1,13 @@
+package com.example.cruddatabasekata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudDatabaseKataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudDatabaseKataApplication.class, args);
+	}
+
+}
---
+++ back-crud-database-kata/src/test/java/com/example/cruddatabasekata/CrudDatabaseKataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cruddatabasekata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudDatabaseKataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
